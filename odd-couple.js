7f15a98f52490bf846a51609850b4cbefeaca386
ryhoneydew <lucia.yang88@gmail.com>
2018-08-04 16:26:29 -0400

    remove js file

--- odd-couple.js
+++
@@ -1,8 +0,0 @@
-// YOUR CODE BELOW
-const oddCouple = (aArray) => {
-    let oddNumberArray = [];
-    oddNumberArray = aArray.filter(num => num %2 != 0);
-    if(oddNumberArray.length === 0) return [];
-    return oddNumberArray.slice(0,2);
-}
-cons juyd
